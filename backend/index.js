6fc98a15cc8f0746bd85d109825c23680696b763
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-07-19 11:21:04 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
+
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors({
   origin:"http://localhost:5173",
@@ -19,6 +22,13 @@ app.use(cookieParser());
 const port = process.env.PORT || 5000;
 
 app.use("/api/auth",authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"/frontend/dist")));
+  app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+  })
+}
 
 app.listen(port,()=>{
   connectDB();
